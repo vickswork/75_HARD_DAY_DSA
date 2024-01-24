ff0784df9479fe6523d0b925b33d7a777fb6db07
Aryan Negi <113013311+vickswork@users.noreply.github.com>
2024-01-24 22:48:33 +0530

    MergetwoSortedArraysWithoutExtraSpace.cpp

---
+++ MergetwoSortedArraysWithoutExtraSpace.cpp
@@ -0,0 +1,22 @@
+void merge(long long arr1[], long long arr2[], int n, int m) {
+
+    //Declare 2 pointers:
+    int left = n - 1;
+    int right = 0;
+
+    //Swap the elements until arr1[left] is
+    // smaller than arr2[right]:
+    while (left >= 0 && right < m) {
+        if (arr1[left] > arr2[right]) {
+            swap(arr1[left], arr2[right]);
+            left--, right++;
+        }
+        else {
+            break;
+        }
+    }
+
+    // Sort arr1[] and arr2[] individually:
+    sort(arr1, arr1 + n);
+    sort(arr2, arr2 + m);
+}
